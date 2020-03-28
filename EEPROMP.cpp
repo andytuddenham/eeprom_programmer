05da1a0cded073f4016a7339e761c03caa7bbcfc
Mark Tuddenham <mark@tudders.com>
2020-03-28 22:21:03 +0000

    formatting

--- EEPROMP.cpp
+++ EEPROMP.cpp
@@ -1,13 +1,17 @@
 #include "EEPROMP.h"
 
-EEPROMP& EEPROMP::getEeprom() {
+EEPROMP &EEPROMP::getEeprom()
+{
   static EEPROMP _eeprom;
   return _eeprom;
 }
 
-bool EEPROMP::setChip(Chip chip) {
-  for (int i = 0; i < sizeof(_chipAddressLines)/sizeof(_chipAddressLines[0]); i++) {
-    if (_chipAddressLines[i][0] == chip) {
+bool EEPROMP::setChip(Chip chip)
+{
+  for (int i = 0; i < sizeof(_chipAddressLines) / sizeof(_chipAddressLines[0]); i++)
+  {
+    if (_chipAddressLines[i][0] == chip)
+    {
       setAddressLines(_chipAddressLines[chip][1]);
       return true;
     }
@@ -15,9 +19,11 @@ bool EEPROMP::setChip(Chip chip) {
   return false;
 }
 
-bool EEPROMP::setAddressLines(byte numAddressLines) {
-  if (_endAddress != 0) return false;
-  _endAddress = pow(2, (float)numAddressLines)-1;
+bool EEPROMP::setAddressLines(byte numAddressLines)
+{
+  if (_endAddress != 0)
+    return false;
+  _endAddress = pow(2, (float)numAddressLines) - 1;
 
   digitalWrite(SR_DATA, LOW);
   pinMode(SR_DATA, OUTPUT);
@@ -31,30 +37,36 @@ bool EEPROMP::setAddressLines(byte numAddressLines) {
   digitalWrite(EEP_OE, HIGH);
   pinMode(EEP_OE, OUTPUT);
 
-  digitalWrite(EEP_WE, HIGH);  // make sure WE is disabled before setting pin mode to OUTPUT
+  digitalWrite(EEP_WE, HIGH); // make sure WE is disabled before setting pin mode to OUTPUT
   pinMode(EEP_WE, OUTPUT);
 }
 
-void EEPROMP::latchAddress() const {
+void EEPROMP::latchAddress() const
+{
   digitalWrite(SR_LATCH, LOW);
   digitalWrite(SR_LATCH, HIGH);
   digitalWrite(SR_LATCH, LOW);
 }
 
-void EEPROMP::setAddress(uint16_t address) const {
-  shiftOut(SR_DATA, SR_CLOCK, LSBFIRST, (uint8_t) address);
+void EEPROMP::setAddress(uint16_t address) const
+{
+  shiftOut(SR_DATA, SR_CLOCK, LSBFIRST, (uint8_t)address);
   shiftOut(SR_DATA, SR_CLOCK, LSBFIRST, (uint8_t)(address >> 8));
   latchAddress();
 }
 
-void EEPROMP::setDataPinsTo(uint8_t mode) const {
-  for (uint8_t pin = DATA_BIT0; pin <= DATA_BIT7; pin++) {
+void EEPROMP::setDataPinsTo(uint8_t mode) const
+{
+  for (uint8_t pin = DATA_BIT0; pin <= DATA_BIT7; pin++)
+  {
     pinMode(pin, mode);
   }
 }
 
-bool EEPROMP::readByte(uint16_t address, byte* data) const {
-  if (_endAddress == 0) return false;
+bool EEPROMP::readByte(uint16_t address, byte *data) const
+{
+  if (_endAddress == 0)
+    return false;
 
   // prepare to read
   setDataPinsTo(INPUT);
@@ -64,26 +76,32 @@ bool EEPROMP::readByte(uint16_t address, byte* data) const {
 
   // read the byte value one bit at a time
   *data = 0x00;
-  for (uint8_t pin = DATA_BIT7; pin >= DATA_BIT0; pin--) {
-    *data |= digitalRead(pin) << pin-DATA_BIT0;
+  for (uint8_t pin = DATA_BIT7; pin >= DATA_BIT0; pin--)
+  {
+    *data |= digitalRead(pin) << pin - DATA_BIT0;
   }
 
   digitalWrite(EEP_OE, HIGH);
   return true;
 }
 
-bool EEPROMP::readArray(uint16_t startAddress, byte* data, int size) const {
-  if (_endAddress == 0) return false;
+bool EEPROMP::readArray(uint16_t startAddress, byte *data, int size) const
+{
+  if (_endAddress == 0)
+    return false;
   uint16_t address = startAddress;
-  for (int offset = 0; offset < size; offset++) {
-    if (!readByte(address+offset, &data[offset])) {
+  for (int offset = 0; offset < size; offset++)
+  {
+    if (!readByte(address + offset, &data[offset]))
+    {
       return false;
     }
   }
   return true;
 }
 
-void EEPROMP::pollTillWriteComplete(byte bit7Value) const {
+void EEPROMP::pollTillWriteComplete(byte bit7Value) const
+{
   // change I/O7 to input and turn output enable on
   pinMode(DATA_BIT7, INPUT);
   digitalWrite(EEP_OE, LOW);
@@ -91,14 +109,17 @@ void EEPROMP::pollTillWriteComplete(byte bit7Value) const {
 
   // reading I/O7 will return the opposite of what was written
   // until the write is complete
-  while (bit7Value != digitalRead(DATA_BIT7)) {
+  while (bit7Value != digitalRead(DATA_BIT7))
+  {
     delayMicroseconds(1);
   }
   delayMicroseconds(1);
 }
 
-bool EEPROMP::writeByte(uint16_t address, byte data) const {
-  if (_endAddress == 0) return false;
+bool EEPROMP::writeByte(uint16_t address, byte data) const
+{
+  if (_endAddress == 0)
+    return false;
 
   // prepare to write
   digitalWrite(EEP_OE, HIGH);
@@ -107,9 +128,11 @@ bool EEPROMP::writeByte(uint16_t address, byte data) const {
 
   // send the byte value one bit at a time
   byte bit7Value;
-  for (uint8_t pin = DATA_BIT0; pin <= DATA_BIT7; pin++) {
+  for (uint8_t pin = DATA_BIT0; pin <= DATA_BIT7; pin++)
+  {
     digitalWrite(pin, data & 1);
-    if (pin == DATA_BIT7) {
+    if (pin == DATA_BIT7)
+    {
       bit7Value = data & 1;
     }
     data = data >> 1;
@@ -125,22 +148,30 @@ bool EEPROMP::writeByte(uint16_t address, byte data) const {
   return true;
 }
 
-bool EEPROMP::writeArray(uint16_t address, byte* data, int size) const {
-  if (_endAddress == 0) return false;
-  for (int offset = 0; offset < size; offset++) {
-    if (!writeByte(address+offset, data[offset])) {
+bool EEPROMP::writeArray(uint16_t address, byte *data, int size) const
+{
+  if (_endAddress == 0)
+    return false;
+  for (int offset = 0; offset < size; offset++)
+  {
+    if (!writeByte(address + offset, data[offset]))
+    {
       return false;
     }
   }
   return true;
 }
 
-bool EEPROMP::printContents(void (*callback)(uint16_t, byte*, int)) const {
-  if (_endAddress == 0) return false;
+bool EEPROMP::printContents(void (*callback)(uint16_t, byte *, int)) const
+{
+  if (_endAddress == 0)
+    return false;
 
-  for (uint16_t base = 0; base <= _endAddress; base +=16) {
+  for (uint16_t base = 0; base <= _endAddress; base += 16)
+  {
     byte data[16] = {0};
-    if (!readArray(base, data, sizeof(data))) return false;
+    if (!readArray(base, data, sizeof(data)))
+      return false;
     (*callback)(base, data, sizeof(data));
   }
 
--- EEPROMP.h
+++ EEPROMP.h
@@ -1,55 +1,57 @@
 #ifndef _EEPROMP_
 #define _EEPROMP_
 
-#if (ARDUINO >= 100 )
-  #include "Arduino.h"
+#if (ARDUINO >= 100)
+#include "Arduino.h"
 #else
-  #include "WProgram.h"
+#include "WProgram.h"
 #endif
 
-class EEPROMP {
-  public:
-    enum Chip: byte {
-      // used to index an array, so must start with zero
-      // and increment sequentially (the default).
-      AT28C64,
-      AT28C256
-    };
-    static EEPROMP& getEeprom();
-    bool setChip(Chip);
-    bool readByte(uint16_t address, byte* data) const;
-    bool readArray(uint16_t startAddress, byte* data, int size) const;
-    bool writeByte(uint16_t address, byte data) const;
-    bool writeArray(uint16_t startAddress, byte* data, int size) const;
-    bool printContents(void (*)(uint16_t address, byte* data, int dataSize)) const;
+class EEPROMP
+{
+public:
+  enum Chip : byte
+  {
+    // used to index an array, so must start with zero
+    // and increment sequentially (the default).
+    AT28C64,
+    AT28C256
+  };
+  static EEPROMP &getEeprom();
+  bool setChip(Chip);
+  bool readByte(uint16_t address, byte *data) const;
+  bool readArray(uint16_t startAddress, byte *data, int size) const;
+  bool writeByte(uint16_t address, byte data) const;
+  bool writeArray(uint16_t startAddress, byte *data, int size) const;
+  bool printContents(void (*)(uint16_t address, byte *data, int dataSize)) const;
 
-    EEPROMP(EEPROMP const&) = delete;
-    void operator=(EEPROMP const&) = delete;
+  EEPROMP(EEPROMP const &) = delete;
+  void operator=(EEPROMP const &) = delete;
 
-  private:
-    // SR_ Shift Register pins
-    static const uint8_t SR_CLOCK = A2;
-    static const uint8_t SR_LATCH = A3;
-    static const uint8_t SR_DATA = 10;
+private:
+  // SR_ Shift Register pins
+  static const uint8_t SR_CLOCK = A2;
+  static const uint8_t SR_LATCH = A3;
+  static const uint8_t SR_DATA = 10;
 
-    // EEP_ eeprom control pins
-    static const uint8_t EEP_OE = A4;
-    static const uint8_t EEP_WE = A5;
+  // EEP_ eeprom control pins
+  static const uint8_t EEP_OE = A4;
+  static const uint8_t EEP_WE = A5;
 
-    // DATA_ eeprom data pins - must be consecutive ascending pins
-    static const uint8_t DATA_BIT0 = 2;
-    static const uint8_t DATA_BIT7 = 9;
+  // DATA_ eeprom data pins - must be consecutive ascending pins
+  static const uint8_t DATA_BIT0 = 2;
+  static const uint8_t DATA_BIT7 = 9;
 
-    int _endAddress = 0;
-    byte _chipAddressLines[2][2] = {
-        {AT28C64, 13},  // ATMEL 64K Parallel eeprom (8K x 8)
-        {AT28C256, 15}  // ATMEL 256K Parallel eeprom (32K x 8)
-    };
-    EEPROMP(){};
-    bool setAddressLines(byte numAddressLines);
-    void latchAddress() const;
-    void setAddress(uint16_t) const ;
-    void setDataPinsTo(uint8_t) const;
-    void pollTillWriteComplete(byte) const;
+  int _endAddress = 0;
+  byte _chipAddressLines[2][2] = {
+      {AT28C64, 13}, // ATMEL 64K Parallel eeprom (8K x 8)
+      {AT28C256, 15} // ATMEL 256K Parallel eeprom (32K x 8)
+  };
+  EEPROMP(){};
+  bool setAddressLines(byte numAddressLines);
+  void latchAddress() const;
+  void setAddress(uint16_t) const;
+  void setDataPinsTo(uint8_t) const;
+  void pollTillWriteComplete(byte) const;
 };
 #endif
