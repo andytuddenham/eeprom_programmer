54fa2faffea96aa6629df6caa0e6089784c2c554
Mark Tuddenham <mark@tudders.com>
2020-03-29 13:39:42 +0100

    Update the printData callback.

--- example/example.ino
+++ example/example.ino
@@ -17,31 +17,30 @@ void setup()
         //         data[8], data[9], data[10], data[11],
         //         data[12], data[13], data[14], data[15]);
         // Serial.println(buf);
-        printContents(addr, data, 16);
+        printData(addr, data, 16);
 
         byte newData[] = {0x0f, 0x1e, 0x2d, 0x3c, 0xf0, 0xe1, 0xd2, 0xc3};
         eeprom.writeArray(addr, newData, sizeof(newData));
         Serial.println("Reading EEPROM");
-        eeprom.printContents(printContents);
+        eeprom.printContents(printData);
         Serial.println("Done.");
     }
 }
 
-void printContents(uint16_t address, byte *data, size_t dataSize)
+void printData(uint16_t address, byte *data, size_t dataSize)
 {
-    //  "%04x: %02x%02x%02x%02x %02x%02x%02x%02x %02x%02x%02x%02x %02x%02x%02x%02x"
-    size_t buffSize = 6 /*"%04x: "*/ + (2 * dataSize) /*"%02x"*/ + (dataSize / 4) /*" "*/ + 1 /*null string termination*/;
+    //  %04x: %02x%02x%02x%02x %02x%02x%02x%02x %02x%02x%02x%02x %02x%02x%02x%02x
+    size_t buffSize = 5 /*"%04x:"*/ + (2 * dataSize) /*"%02x"*/ + (dataSize / 4) /*" "*/ + 1 /*null string termination*/;
     char buff[buffSize];
 
-    sprintf(buff, "%04x: ", address);
+    sprintf(buff, "%04x:", address);
     for (int i = 0; i < dataSize; i++)
     {
+        if (i % 4 == 0)
+            strcat(buff, " ");
+
         char *nextPtr = buff + strlen(buff);
         sprintf(nextPtr, "%02x", data[i]);
-        if ((i + 1) % 4 == 0)
-        {
-            strcat(buff, " ");
-        }
     }
 
     Serial.println(buff);
