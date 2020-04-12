1e6114d9197de4d4d0c7ffbeb469b51886ff847b
Mark Tuddenham <mark@tudders.com>
2020-04-12 19:10:20 +0100

    Add helper functions
    Helper functions to read and write arrays and different sized types

--- examples/basic/basic.ino
+++ examples/basic/basic.ino
@@ -14,12 +14,48 @@ void setup()
   Serial.print("EEPROM (1): ");
   Serial.println(static_cast<int>(eeprom[1]), HEX);
 
-  for (byte b: eeprom)
-  {
-    Serial.println(b);
-  }
+  // for (byte b: eeprom)
+  // {
+  //   Serial.println(b, HEX);
+  // }
+
+  uint16_t addr = 0xef;
+  byte new_data[] = {0x0f, 0x1e, 0x2d, 0x3c, 0xf0, 0xe1, 0xd2, 0xc3};
+  eeprom.set_array(addr, new_data);
+
+  byte read_data[16];
+  eeprom.get_array(addr, read_data);
+  printData(addr, read_data, sizeof(read_data));
+
+  addr = 0;
+  // printData(addr, &eeprom.get<byte>(addr), 1);
+  byte read_data2[64];
+  eeprom.get_array(addr, read_data2);
+  printData(addr, read_data2, sizeof(read_data2));
+
+  eeprom.get_content_in_chunks(64, printData);
 
   Serial.println("Done");
+}
+
+void printData(uint16_t address, byte* data, size_t dataSize)
+{
+  //  %04x: %02x%02x%02x%02x %02x%02x%02x%02x %02x%02x%02x%02x %02x%02x%02x%02x
+  size_t buffSize = 5 /*"%04x:"*/ + ceil(static_cast<float>(dataSize) / 4) /*" "*/
+                    + (2 * dataSize) /*"%02x"*/ + 1 /*null string termination*/;
+  char buff[buffSize];
+
+  sprintf(buff, "%04x:", address);
+  for (size_t i = 0; i < dataSize; i++)
+  {
+    if (i % 4 == 0)
+      strcat(buff, " ");
+
+    char* nextPtr = buff + strlen(buff);
+    sprintf(nextPtr, "%02x", data[i]);
+  }
+
+  Serial.println(buff);
 }
 
 void loop() {}
--- src/EEPROM.hpp
+++ src/EEPROM.hpp
@@ -13,18 +13,98 @@ class EEPROM
 public:
   EEPROM() = delete;
   EEPROM(const Config& c) : m_interface(c){};
-  byte read(const size_t index) { return static_cast<byte>(new_ref(index)); }
-  void write(const size_t index, const byte val) { new_ref(index) = val; }
+
+  byte read(const size_t addr) { return static_cast<byte>(new_ref(addr)); }
+  void write(const size_t addr, const byte val) { new_ref(addr) = val; }
+
   Iter begin() { return new_iter(0); }
   Iter end() { return new_iter(size()); }
-  Ref operator[](const size_t index) { return new_ref(index); }
 
+  Ref operator[](const size_t addr) { return new_ref(addr); }
   size_t size() { return m_interface.m_config.size; }
+
+  void erase() { fill(0xff); }
+
+  void fill(uint8_t val)
+  {
+    for (Ref ref: *this)
+    {
+      ref = val;
+    }
+  }
+
+  template<typename T>
+  T get(const size_t addr)
+  {
+    T data;
+    get(addr, data);
+    return data;
+  }
+
+  template<typename T>
+  void get(const size_t addr, T& t)
+  {
+    Iter iter = new_iter(addr);
+    uint8_t* data_ptr = reinterpret_cast<uint8_t*>(&t);
+    for (size_t b = 0; b < sizeof(T); ++b)
+    {
+      *data_ptr++ = *iter++;
+    }
+  }
+
+  template<typename T>
+  void get_array(const size_t addr, T* data, size_t size)
+  {
+    for (size_t i = 0; i < size; ++i)
+    {
+      get(addr + (i * sizeof(T)), data[i]);
+    }
+  }
+
+  template<typename T, size_t N>
+  void get_array(const size_t addr, T (&data)[N])
+  {
+    for (size_t i = 0; i < N; ++i)
+    {
+      get(addr + (i * sizeof(T)), data[i]);
+    }
+  }
+
+  template<typename T>
+  void set(const size_t addr, const T& t)
+  {
+    Iter iter = new_iter(addr);
+    const uint8_t* data_ptr = reinterpret_cast<const uint8_t*>(&t);
+    for (size_t b = 0; b < sizeof(T); ++b)
+    {
+      *iter++ = *data_ptr++;
+    }
+  }
+
+  template<typename T, size_t N>
+  void set_array(const size_t addr, T (&data)[N])
+  {
+    for (size_t i = 0; i < N; ++i)
+    {
+      set(addr + (i * sizeof(T)), data[i]);
+    }
+  }
+
+  template<typename F>
+  void get_content_in_chunks(size_t chunk_size, F& f)
+  {
+    for (uint16_t addr = 0; addr <= size() - chunk_size; addr += chunk_size)
+    {
+      byte data[chunk_size] = {0};
+      get_array(addr, data, chunk_size);
+      f(addr, data, chunk_size);
+    }
+  }
 
 private:
   const EEPROM_interface m_interface;
-  Ref new_ref(size_t index) const { return Ref(m_interface, index); }
-  Iter new_iter(size_t index) const { return Iter(m_interface, index); }
+  Ref new_ref(size_t addr) const { return Ref(m_interface, addr); }
+  Iter new_iter(size_t addr) const { return Iter(m_interface, addr); }
 };
 
 } // namespace ext_memory
